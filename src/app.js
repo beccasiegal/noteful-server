b1b60c22718d37520b29663c2c155c72fe751d87
Rebecca <becca.siegal@gmail.com>
2020-02-20 13:07:13 -0500

    Added error logging for prod

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ app.use('/api/folders', foldersRouter)
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
+        console.log(error.message);
         response = { error: { message: 'server error' } }
     } else {
         console.error(error)
