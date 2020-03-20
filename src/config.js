275ba7d4a023874b6995f40477bf922c84e23ca9
Rebecca <becca.siegal@gmail.com>
2020-03-20 11:45:53 -0400

    updated config.js

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ module.exports = {
 	NODE_ENV: process.env.NODE_ENV || "development",
 	DATABASE_URL:
 		process.env.DATABASE_URL ||
-		"postgres://localhost/noteful",
+		"postgres://rebecca@localhost/noteful",
 	TEST_DATABASE_URL:
 		process.env.TEST_DATABASE_URL ||
-		"postgres://localhost/noteful-test"
+		"postgres://rebecca@localhost/noteful-test"
 };
