71a40e26f36afd31a7fe39d1de4f2643816a34b7
Rebecca <becca.siegal@gmail.com>
2020-03-28 10:35:53 -0400

    Added notes-router

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require('./config')
 const foldersRouter = require('./folders/folders-router');
+const notesRouter = require('./notes/notes-router');
 
 const app = express();
 
@@ -27,6 +28,18 @@ app.use(function errorHandler(error, req, res, next) {
     res.status(500).json(response)
 })
 
+app.use('/api/notes', notesRouter)
+app.use(function errorHandler(error, req, res, next) {
+    let response
+    if (NODE_ENV === 'production') {
+        console.log(error.message);
+        response = { error: { message: 'server error' } }
+    } else {
+        console.error(error)
+        response = { message: error.message, error }
+    }
+    res.status(500).json(response)
+})
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
