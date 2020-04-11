e7343842bed299a80e1bed6af1541ea437025e43
Rebecca <becca.siegal@gmail.com>
2020-04-11 17:11:14 -0400

    updated .env

--- src/server.js
+++ src/server.js
@@ -5,11 +5,12 @@ console.log("Database url", DATABASE_URL);
 const db = knex({
 	client: 'pg',
 	connection: {
-	host: 'ec2-18-209-187-54.compute-1.amazonaws.com',
-	user: 'biiluakxlfaobs',
-	password: '1271cba11e542174f480ca512a51c3053e4b116565cff86909f1802f6cf8b4ee',
-	database: 'd7sf6uk349u75g',
-	ssl: true}
+		host: process.env.HOST,
+		user: process.env.USER,
+		password: process.env.PASSWORD,
+		database: process.env.DATABASE,
+		ssl: true
+	}
 })
 
 app.set('db', db)
