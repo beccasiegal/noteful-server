9e5015be90d6447383c533f3f243a86def6fff39
Rebecca <becca.siegal@gmail.com>
2020-04-11 13:30:17 -0400

    Updated server listening at url

--- src/server.js
+++ src/server.js
@@ -11,5 +11,5 @@ const db = knex({
 app.set('db', db)
 
 app.listen(PORT, () => {
-	console.log(`Server listening at http://localhost:${PORT}`);
+	console.log(`Server listening at http://ec2-18-209-187-54.compute-1.amazonaws.com:${PORT}`);
 });
