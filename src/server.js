24cc2c99cf8dc2cfb1ffffe8fd69342270a6b703
Rebecca <becca.siegal@gmail.com>
2020-04-11 12:53:22 -0400

    fixed missing comma in server.js

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const { PORT, DATABASE_URL } = require('./config')
 console.log("Database url", DATABASE_URL);
 const db = knex({
 	client: 'pg',
-	connection: DATABASE_URL
+	connection: DATABASE_URL,
 	ssl: true
 })
 
