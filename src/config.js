be5350b137ac1fec9c888d5b28e6d391d87dca30
Rebecca <becca.siegal@gmail.com>
2020-04-11 16:22:24 -0400

    Updated server.js connection object

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	PORT: process.env.PORT || 8000,
 	NODE_ENV: process.env.NODE_ENV || "development",
-	DATABASE_URL:
-		process.env.DATABASE_URL ||
-		DATABASE_URL="postgresql://biiluakxlfaobs:1271cba11e542174f480ca512a51c3053e4b116565cff86909f1802f6cf8b4ee@ec2-18-209-187-54.compute-1.amazonaws.com/d7sf6uk349u75g",
+	DATABASE_URL: 
+		process.env.DATABASE_URL || 
+		"postgresql://biiluakxlfaobs:1271cba11e542174f480ca512a51c3053e4b116565cff86909f1802f6cf8b4ee@ec2-18-209-187-54.compute-1.amazonaws.com/d7sf6uk349u75g",
 	TEST_DATABASE_URL:
 		process.env.TEST_DATABASE_URL ||
 		"postgresql://rebecca@localhost/noteful-test",
--- src/server.js
+++ src/server.js
@@ -4,8 +4,12 @@ const { PORT, DATABASE_URL } = require('./config')
 console.log("Database url", DATABASE_URL);
 const db = knex({
 	client: 'pg',
-	connection: DATABASE_URL,
-	ssl: true
+	connection: {
+	host: 'ec2-18-209-187-54.compute-1.amazonaws.com',
+	user: 'biiluakxlfaobs',
+	password: '1271cba11e542174f480ca512a51c3053e4b116565cff86909f1802f6cf8b4ee',
+	database: 'd7sf6uk349u75g',
+	ssl: true}
 })
 
 app.set('db', db)
